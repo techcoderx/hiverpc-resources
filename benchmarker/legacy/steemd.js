b92bfcc823571d354e309128f0688411aea311c5
techcoderx <git@techcoderx.com>
2020-11-25 17:18:27 +0800

    node benchmarker + jussi config

---
+++ benchmarker/legacy/steemd.js
@@ -0,0 +1,44 @@
+const steem = require('steem')
+
+steem.api.setOptions({ url: 'http://192.168.0.186:8093', useAppbaseApi: true })
+
+let user = 'techcoderx'
+
+let startTime = new Date().getTime()
+let lastTime = new Date().getTime()
+
+console.log()
+console.log('Welcome to AppBase steemd API Benchmark')
+console.log()
+
+steem.api.getDynamicGlobalProperties(() => {
+    console.log('getDynamicGlobalProperties: ' + (new Date().getTime() - lastTime) + 'ms')
+    setLastTime()
+    steem.api.getConfig(() => {
+        console.log('getConfig: ' + (new Date().getTime() - lastTime) + 'ms')
+        setLastTime()
+        steem.api.getAccounts([user],() => {
+            console.log('getAccounts: ' + (new Date().getTime() - lastTime) + 'ms')
+            setLastTime()
+            steem.api.getAccountHistory(user,-1,100,() => {
+                console.log('getAccountHistory: ' + (new Date().getTime() - lastTime) + 'ms')
+                setLastTime()
+                steem.api.getOrderBook(20, () => {
+                    console.log('getOrderBook: ' + (new Date().getTime() - lastTime) + 'ms')
+                    setLastTime()
+                    endBenchmark()
+                })
+            })
+        })
+    })
+})
+
+function setLastTime() {
+    lastTime = new Date().getTime()
+}
+
+function endBenchmark() {
+    console.log()
+    console.log('Total benchmark time: ' + (lastTime - startTime) + 'ms')
+    console.log()
+}
