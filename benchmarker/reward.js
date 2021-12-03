43dfca2ed7e63fbdc6673d2ec1a34b2cf661f39f
techcoderx <git@techcoderx.com>
2021-12-03 21:22:31 +0800

    script for retrieving producer rewards for x days

---
+++ benchmarker/reward.js
@@ -0,0 +1,38 @@
+const hive = require('@hiveio/hive-js')
+const producer_reward_op = require('@hiveio/hive-js/lib/auth/serializer').makeBitMaskFilter([require('@hiveio/hive-js/lib/auth/serializer/src/ChainTypes').operations.producer_reward])
+const witness = process.argv[2]
+const days = parseInt(process.argv[3])
+
+if (!witness || witness === '--help' || witness === '-help' || witness === '-h' || isNaN(days)) {
+    console.log('Usage: node reward.js <witness> <days>')
+    process.exit(0)
+}
+
+const currentTs = new Date().getTime()
+const durationMs = 1000*60*60*24*(days)
+
+const getProducerRewards = (last, cb, total = 0) => {
+    hive.api.getAccountHistory(witness,last,1000,...producer_reward_op,(e,h) => {
+        if (e)
+            console.log(e)
+        else
+            console.log('fetched',h.length,'ops')
+        for (let i = h.length - 1; i >= 0; i--)
+            if ((currentTs - durationMs) < new Date(h[i][1].timestamp).getTime())
+                total += parseFloat(h[0][1].op[1].vesting_shares)
+        if ((currentTs - durationMs) < new Date(h[0][1].timestamp).getTime())
+            getProducerRewards(h[0][0]-1,cb,total)
+        else
+            cb(Math.round(total*1000000)/1000000)
+    })
+}
+
+getProducerRewards(-1,(vestProduced) => {
+    console.log('Vests Produced:',vestProduced)
+    hive.api.getDynamicGlobalProperties((e,props) => {
+        if (e)
+            console.log(e)
+        else
+            console.log(hive.formatter.vestToHive(vestProduced,props.total_vesting_shares,props.total_vesting_fund_hive),'HIVE')
+    })
+})
