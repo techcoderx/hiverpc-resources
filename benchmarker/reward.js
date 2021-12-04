13fa87528850c2013093019e5d224533a042924b
techcoderx <git@techcoderx.com>
2021-12-04 08:55:50 +0800

    display block count

--- benchmarker/reward.js
+++ benchmarker/reward.js
@@ -11,23 +11,27 @@ if (!witness || witness === '--help' || witness === '-help' || witness === '-h' 
 const currentTs = new Date().getTime()
 const durationMs = 1000*60*60*24*(days)
 
-const getProducerRewards = (last, cb, total = 0) => {
+const getProducerRewards = (last, cb, total = 0, count = 0) => {
     hive.api.getAccountHistory(witness,last,1000,...producer_reward_op,(e,h) => {
-        if (e)
+        if (e) {
             console.log(e)
-        else
+            return cb(Math.round(total*1000000)/1000000,count)
+        } else
             console.log('fetched',h.length,'ops')
         for (let i = h.length - 1; i >= 0; i--)
-            if ((currentTs - durationMs) < new Date(h[i][1].timestamp).getTime())
+            if ((currentTs - durationMs) < new Date(h[i][1].timestamp).getTime()) {
                 total += parseFloat(h[0][1].op[1].vesting_shares)
+                count++
+            }
         if ((currentTs - durationMs) < new Date(h[0][1].timestamp).getTime())
-            getProducerRewards(h[0][0]-1,cb,total)
+            getProducerRewards(h[0][0]-1,cb,total,count)
         else
-            cb(Math.round(total*1000000)/1000000)
+            cb(Math.round(total*1000000)/1000000,count)
     })
 }
 
-getProducerRewards(-1,(vestProduced) => {
+getProducerRewards(-1,(vestProduced,count) => {
+    console.log('Blocks Produced:',count)
     console.log('Vests Produced:',vestProduced)
     hive.api.getDynamicGlobalProperties((e,props) => {
         if (e)
