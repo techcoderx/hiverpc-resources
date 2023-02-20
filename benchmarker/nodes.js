6435ed8c3a9922f02a9eca6b1b52fb3091e025b8
techcoderx <git@techcoderx.com>
2023-02-20 10:08:07 +0800

    organize

--- benchmarker/nodes.js
+++ benchmarker/nodes.js
@@ -2,14 +2,20 @@ module.exports = [
     'https://api.hive.blog',
     'https://api.openhive.network',
     'https://anyx.io',
-    'https://api.pharesim.me',
-    'https://rpc.ausbit.dev',
-    'https://rpc.ecency.com',
     'https://hive.roelandp.nl',
+    'https://rpc.ecency.com',
+    'https://rpc.mahdiyari.info',
     'https://api.c0ff33a.uk',
     'https://api.deathwing.me',
     'https://api.hive.blue',
     'https://hive-api.arcange.eu',
+    'https://hive-api.3speak.tv',
     'https://hived.emre.sh',
-    'https://techcoderx.com'
+    'https://hiveapi.actifit.io',
+    'https://techcoderx.com',
+
+    // unreachable at time of report
+    // 'https://api.pharesim.me',
+    // 'https://rpc.ausbit.dev',
+    // 'https://hived.privex.io'
 ]
